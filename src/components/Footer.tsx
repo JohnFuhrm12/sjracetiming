0d976624cc06a5c74e6b9db8b1c944a7243fa870
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-05 01:29:54 -0400

    Added Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,21 @@ import '../styles/footer.css';
 function Footer() {
     return (
         <>
-            <h1>Footer</h1>
+            <div id='footerWrapper'>
+                <div id='sponsorImgWrapper'>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693891296/SJ%20Race%20Timing/lm9mg9nllahm0ecuctlz.jpg' className='sponsorImg' alt='Pineland Striders'/>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693891297/SJ%20Race%20Timing/unaejbz9ggqv90fnk4ig.png' className='sponsorImg' alt='Pineland Striders'/>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693880283/SJ%20Race%20Timing/gyrk7mzuutrns1ntgkvx.webp' className='sponsorImg' alt='Pineland Striders'/>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693891297/SJ%20Race%20Timing/hmydnvu0dbf8k1jgdvdn.webp' className='sponsorImg' alt='Pineland Striders'/>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693891300/SJ%20Race%20Timing/tdluswydzb7gn7k7iihd.webp' className='sponsorImg' alt='Pineland Striders'/>
+                    <img src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693891301/SJ%20Race%20Timing/kndzr2lvtzlnuh0ninac.webp' className='sponsorImg' alt='Pineland Striders'/>
+                </div>
+                <div id='footerLinksSmallWrapper'>
+                    <a className='footerLinkSmall'>Terms and Conditions</a>
+                    <a className='footerLinkSmall'>Privacy Policy</a>
+                </div>
+                <p id='copyright'>'Copyright © 2023 | South Jersey Race Timing LLC | All Rights Reserved</p>
+            </div>
         </>
     )
 }
