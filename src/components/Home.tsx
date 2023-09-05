2966b317febfbdb5de4b83acf187588e07c6b224
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-05 01:11:06 -0400

    Added hamburger nav + mobile styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,19 @@ function Home() {
     return (
         <>
             <div id='homePageBackground'>
-
-            </div>
             <div id='buttonWrapper'>
                 <button className='button'>Find a Race</button>
                 <button className='button'>See Results</button>
             </div>
-            <h1 id='title' className='fade'><span id='pinkTitle'>About</span> South Jersey Race Timing</h1>
+            </div>
+            <div className='infoBlock fade'>
+                <h1 id='title'><span id='pinkTitle'>About</span> South Jersey Race Timing</h1>
+                <p className='infoParagraph'>At South Jersey Race Timing, we believe that your success is job number one. For over 25 years we have worked and participated in the industry as race directors, race timers, and race participants. As avid runners, we know how accurate timing and organization are so important to a race for both participants and directors.</p>
+            </div>
+            <div className='infoBlock fade'>
+                <p className='infoParagraph'>We offer full service packages from start to finish with advice on our own experience, race set up and application, race timing, race results, and even video/media of every event.</p>
+                <p className='infoParagraph'>We take pride in our work and look forward to speaking with you. Take a look at our services or simply contact us today.</p>
+            </div>
         </>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,26 @@ import '../styles/navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    function MobileNavAnimation() {
+        const hamburgerMenu = document.getElementById('menuCheck') as HTMLInputElement;
+        const mobileNav = document.getElementById('hamburgerNav') as HTMLInputElement;
+
+        if (hamburgerMenu.checked) {
+            mobileNav.style.translate = '0';
+        } else {
+            mobileNav.style.translate = '-100%';
+        }
+
+    }
+
+    function closeNav() {
+        const hamburgerMenu = document.getElementById('menuCheck') as HTMLInputElement;
+        const mobileNav = document.getElementById('hamburgerNav') as HTMLInputElement;
+
+        hamburgerMenu.checked = false;
+        mobileNav.style.translate = '-100%';
+    }
+
     return (
         <>
             <div className='wrapper'>
@@ -15,6 +35,13 @@ function Navbar() {
                     <Link to='/' className='navLink'>Results</Link>
                     <Link to='/' className='navLink'>Contact</Link>
                 </div>
+                <label id='hamburgerMenu' onClick={MobileNavAnimation}>
+                    <input id='menuCheck' type='checkbox'/>
+                </label>
+            </div>
+            <div id='hamburgerNav'>
+                <Link to='/' className='mobileNavLink' onClick={closeNav}>Link1</Link>
+                <Link to='/' className='mobileNavLink' onClick={closeNav}>Link2</Link>
             </div>
         </>
     )
