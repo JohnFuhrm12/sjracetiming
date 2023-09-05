728241c6532916e38a24191ea39aaba5f9bdb060
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-04 23:58:31 -0400

    Added homepage image, header, finished navbar, and added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,25 @@ import PageNotFound from './components/PageNotFound';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+
+  window.addEventListener('scroll', fadeIn);
+
+  function fadeIn() {
+    const fade = document.querySelectorAll('.fade')
+
+    for (let i = 0; i < fade.length; i++) {
+      const windowheight = window.innerHeight;
+      const revealtop = fade[i].getBoundingClientRect().top;
+      const revealpoint = 10;
+
+      if (revealtop < windowheight - revealpoint) {
+        fade[i].classList.add('active');
+      } else {
+        fade[i].classList.remove('active');
+      }
+    }
+  } 
+  
   return (
     <>
       <Navbar/>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,14 @@ import '../styles/home.css';
 function Home() {
     return (
         <>
-            <h1>SJ Race Timing</h1>
+            <div id='homePageBackground'>
+
+            </div>
+            <div id='buttonWrapper'>
+                <button className='button'>Find a Race</button>
+                <button className='button'>See Results</button>
+            </div>
+            <h1 id='title' className='fade'><span id='pinkTitle'>About</span> South Jersey Race Timing</h1>
         </>
     )
 }
