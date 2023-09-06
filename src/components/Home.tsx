aee5c3b2f8c677e0ea76abac9afffa802dfa20b5
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-06 00:32:15 -0400

    Style Changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -42,8 +42,8 @@ function Home() {
         <>
             <div id='homePageBackground'>
                 <div id='buttonWrapper'>
-                    <button className='button'>Find a Race</button>
-                    <button className='button'>See Results</button>
+                    <button className='button topHome'>FIND A RACE</button>
+                    <button className='button topHome'>SEE RESULTS</button>
                 </div>
                 <div id='socialIconsWrapperHomePage'>
                     <a href="https://www.facebook.com/SouthJerseyRaceTimingLlc/" target='_blank' className="fa fa-facebook"></a>
@@ -51,17 +51,15 @@ function Home() {
                 </div>
             </div>
             <div className='infoBlock fade'>
-                <h1 id='title'><span id='pinkTitle'>About</span> South Jersey Race Timing</h1>
-                <p className='infoParagraph'>At South Jersey Race Timing, we believe that your success is job number one. For over 25 years we have worked and participated in the industry as race directors, race timers, and race participants. As avid runners, we know how accurate timing and organization are so important to a race for both participants and directors.</p>
-            </div>
-            <div className='infoBlock fade'>
-                <p className='infoParagraph'>We offer full service packages from start to finish with advice on our own experience, race set up and application, race timing, race results, and even video/media of every event.</p>
-                <p className='infoParagraph'>We take pride in our work and look forward to speaking with you. Take a look at our services or simply contact us today.</p>
+                <h1 id='title'><span id='pinkTitle'>ABOUT</span> SOUTH JERSEY RACE TIMING</h1>
+                <p className='infoParagraph italicsStyle'>At South Jersey Race Timing, we believe that your success is job number one. For over 25 years we have worked and participated in the industry as race directors, race timers, and race participants. As avid runners, we know how accurate timing and organization are so important to a race for both participants and directors.</p>
+                <p className='infoParagraph italicsStyle'>We offer full service packages from start to finish with advice on our own experience, race set up and application, race timing, race results, and even video/media of every event.</p>
+                <p className='infoParagraph italicsStyle'>We take pride in our work and look forward to speaking with you. Take a look at our services or simply contact us today.</p>
             </div>
             <div className='infoBlockBlue'>
                 <div className='eventsResultsWrapper'>
                     <div id='eventsBlock'>
-                        <h2 className='subtitleBlue'><span className='pinkSubtitle'>Upcoming</span> Events</h2>
+                        <h2 className='subtitleBlue'><span className='pinkSubtitle'>UPCOMING</span> EVENTS</h2>
                         {events.map((event) => {
                             return (
                                 <>
@@ -77,10 +75,10 @@ function Home() {
                                 </>
                             )
                         })}
-                        <Link to='/' className='seeAllLink'>See Full Schedule</Link>
+                        <Link to='/' className='seeAllLink'>SEE FULL SCHEDULE</Link>
                     </div>
                     <div id='resultsBlock'>
-                        <h2 className='subtitleBlue'><span className='pinkSubtitle'>Recent</span> Results</h2>
+                        <h2 className='subtitleBlue'><span className='pinkSubtitle'>RECENT</span> RESULTS</h2>
                         {results.map((result) => {
                             return (
                                 <>
@@ -90,20 +88,20 @@ function Home() {
                                 </>
                             )
                         })}
-                        <Link to='/' className='seeAllLink'>See All Results</Link>
+                        <Link to='/' className='seeAllLink'>SEE ALL RESULTS</Link>
                     </div>
                 </div>
             </div>
             <div className='infoBlock fade'>
-                <h2 className='subtitle'><span className='pinkSubtitle'>Our</span> Services</h2>
-                <p className='infoParagraph'>We provide high quality services to race directors based on our many years of experience. When working with South Jersey Race Timing, our clients receive consultation and advice in addition to our event services. We help every step of the way to ensure your event is the best it can be.</p>
-                <p className='infoParagraph'>A big perk of working with SJRT is our email marketing. We have compiled a list of thousands of runners that are looking for races and eager to get out and compete. This adds a nice kick start to your event. We are also heavily invested in social media with a very busy Facebook page and over 200 subscribers to our YouTube Channel.</p>
-                <p className='infoParagraph'>Our services don't stop when the race is over, we work with all of our clients with post race marketing, and we like to be included in post race meetings to measure event success</p>
+                <h2 className='subtitle'><span className='pinkSubtitle'>OUR</span> SERVICES</h2>
+                <p className='infoParagraph italicsStyle'>We provide high quality services to race directors based on our many years of experience. When working with South Jersey Race Timing, our clients receive consultation and advice in addition to our event services. We help every step of the way to ensure your event is the best it can be.</p>
+                <p className='infoParagraph italicsStyle'>A big perk of working with SJRT is our email marketing. We have compiled a list of thousands of runners that are looking for races and eager to get out and compete. This adds a nice kick start to your event. We are also heavily invested in social media with a very busy Facebook page and over 200 subscribers to our YouTube Channel.</p>
+                <p className='infoParagraph italicsStyle'>Our services don't stop when the race is over, we work with all of our clients with post race marketing, and we like to be included in post race meetings to measure event success</p>
             </div>
             <div className='infoBlockBlue'>
                     <div id='servicesBlocksWrapper'>
                         <div className='servicesInfoWrapper'>
-                            <h2 className='subtitleBlueSmall fade'>Registration & Planning</h2>
+                            <h2 className='subtitleBlueSmall fade'><span className='subtitleBluePinkSmall'>Registration</span> & Planning</h2>
                             <ul className='servicesListUL fade'>
                                 <li className='infoParagraphBlue listItem'>Race Suggestions and Consultation</li>
                                 <li className='infoParagraphBlue listItem'>Web and Print Friendly Application Process</li>
@@ -115,7 +113,7 @@ function Home() {
                             </ul>
                         </div>
                         <div className='servicesInfoWrapper'>
-                            <h2 className='subtitleBlueSmall fade'>Course Set Up & Safety</h2>
+                            <h2 className='subtitleBlueSmall fade'><span className='subtitleBluePinkSmall'>Course</span> Set Up & Safety</h2>
                             <ul className='servicesListUL fade'>
                                 <li className='infoParagraphBlue listItem'>Assist with Course Measurement and Design</li>
                                 <li className='infoParagraphBlue listItem'>Provide Safety Cones, Vests, hand-held flags and Mile Markers free of charge</li>
@@ -126,7 +124,7 @@ function Home() {
                             </ul>
                         </div>
                         <div className='servicesInfoWrapper'>
-                            <h2 className='subtitleBlueSmall fade'>Timing & Post Event</h2>
+                            <h2 className='subtitleBlueSmall fade'><span className='subtitleBluePinkSmall'>Timing</span> & Post Event</h2>
                             <ul className='servicesListUL fade'>
                                 <li className='infoParagraphBlue listItem'>Timing and Results Generated electronically with Chronotrack and BlueBox chip systems</li>
                                 <li className='infoParagraphBlue listItem'>Text results provided within seconds of finishing</li>
@@ -141,9 +139,9 @@ function Home() {
             </div>
             <div id='contactBlockHome'>
                 <div id='contactLogoFormHomeWrapper'>
-                    <img id='logoContactBlock' src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693969389/SJ%20Race%20Timing/g1jzajm3pzy0uzfjyvca.webp' alt='South Jersey Race Timing'/>
-                    <div id='contactFormBlockHome'>
-                        <h2 id='contactHomeTitle'>Contact Us</h2>
+                    <img id='logoContactBlock' className='fade' src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693969389/SJ%20Race%20Timing/g1jzajm3pzy0uzfjyvca.webp' alt='South Jersey Race Timing'/>
+                    <div id='contactFormBlockHome' className='fade'>
+                        <h2 id='contactHomeTitle'>CONTACT US</h2>
                         <form id='contactFormHome'>
                             <input type='text' name='name' className='contactInputHome' placeholder='Full Name'/>
                             <select className='contactSelectHome' name='contactType'>
